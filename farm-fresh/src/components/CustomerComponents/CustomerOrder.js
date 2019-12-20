8b8ebbf765fefab1b15230492dcb9497a957f3a1
oashtari <oashtari@gmail.com>
2019-12-19 20:15:17 -0800

    added test data

--- farm-fresh/src/components/CustomerComponents/CustomerOrder.js
+++ farm-fresh/src/components/CustomerComponents/CustomerOrder.js
@@ -5,6 +5,34 @@ import axios from 'axios';
 import ProduceCard from '../FarmerComponents/ProduceCard';
 import CustomerCard from './CustomerCard';
 
+const produceItems = [
+    {
+        name: 'apple',
+        price: '1',
+        quantity: '19'
+    },
+    {
+        name: 'orange',
+        price: '2',
+        quantity: '8'
+    },
+    {
+        name: 'carrot bunches',
+        price: '3',
+        quantity: '11'
+    },
+    {
+        name: 'guava',
+        price: '5',
+        quantity: '6'
+    },
+    {
+        name: 'coconut',
+        price: '3.5',
+        quantity: '9'
+    }s
+]
+
 export default function CustomerOrder() {
 
     const [order, setOrder] = useState([]);
