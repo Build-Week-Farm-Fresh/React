9b439d36e7b048c208ec0994c5533bbc165240b9
Danica Valdez <danicaevaldez@gmail.com>
2020-01-07 19:41:04 -0800

    test commit

--- farm-fresh/src/App.js
+++ farm-fresh/src/App.js
@@ -14,6 +14,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import CustomerLandginPage from './components/CustomerComponents/CustomerLandingPage';
 
 
+
 function App(props) {
 
   if (props.loginStart) {
