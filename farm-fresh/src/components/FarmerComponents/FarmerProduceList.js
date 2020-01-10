7b42e5b8feb5484a86b0db7de9394de18f41ae6a
Danica Valdez <danicaevaldez@gmail.com>
2020-01-09 18:31:31 -0800

    Can now successfully add, edit, and delete produce

--- farm-fresh/src/components/FarmerComponents/FarmerProduceList.js
+++ farm-fresh/src/components/FarmerComponents/FarmerProduceList.js
@@ -1,16 +1,15 @@
 // id, produce item, produce price, produce quantity
 import React, { useState, useEffect } from "react";
-import { axiosWithAuth } from "../../utils/axiosWithAuth";
-import axios from 'axios';
-import ProduceCard from './ProduceCard';
+import axiosWithAuth from "../../utils/axiosWithAuth";
+import AddProduce from './AddProduce'
+import {Link} from "react-router-dom"
+
 
 export default function FarmerProduceList() {
 
     const [produceList, setProduceList] = useState([]);
 
-
     useEffect(() => {
-
         axiosWithAuth()
             .get('/produce/user/1')
             .then(response => {
@@ -21,22 +20,49 @@ export default function FarmerProduceList() {
             })
     }, []);
 
+    const handleDelete = (e, id) => {
+        e.preventDefault()
+        
+        const produce = produceList.find((produce) => produceList.id === id)
+
+		if (window.confirm("Are you sure you want to delete this item?")) {
+			setProduceList(produceList.filter(produce => produceList.id !== id))
+
+			axiosWithAuth()
+				.delete(`/produce/${id}`)
+				.then(res => {
+                    console.log(res)
+                    axiosWithAuth()
+                    .get('produce/user/1')
+                    .then(res => setProduceList(res.data))
+				})
+				.catch((error) => {
+					console.log(error)
+					setProduceList([...produceList, produce])
+				})
+		}
+    }
+    
     if (!produceList) {
         return <div>Farmer had a sad harvest &#128577 </div>
     }
 
     return (
-        <section className="product-list">
-            <div>
-                <h1>My Produce</h1>
-                <button>Add Produce</button>
-                {produceList.map(produce => {
-                    return (
-                        <ProduceCard ProduceData={produce} key={produce.id} />
-                    )
-                })}
-            </div>
-        </section>
+        <div>
+            <h1>My Produce</h1>
+            <AddProduce setProduceList={setProduceList}/>
+            {produceList.map((produceItem) => (
+                <div key={produceItem.id}>
+                    <Link to={`/editproduce/${produceItem.id}`}>
+                        <button>edit</button>
+                    </Link>
+                    <button onClick={e => handleDelete(e, produceItem.id)}>delete</button> 
+                    <p>{produceItem.name}</p>
+                    <p>{produceItem.price}</p>
+                    <p>{produceItem.quantity}</p>
+                </div>
+            ))}
+        </div>
     );
 }
 
